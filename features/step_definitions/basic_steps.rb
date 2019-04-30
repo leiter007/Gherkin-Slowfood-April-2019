dcbfb6185ce4bbfa04c89807eecbb00e89930582
zanenkn <zane.neikena@gmail.com>
2019-04-30 19:51:33 +0200

    Creates the first feature test for the scenario 'Visitor can see different products on a restaurants menu', creates step definition file.

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,16 @@
+Given("the following products exist") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I visit the landing page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {int}") do |int|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
