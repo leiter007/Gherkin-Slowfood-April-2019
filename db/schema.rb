99a6d6e758e197313989e3f4c1006522121b282e
zanenkn <zane.neikena@gmail.com>
2019-05-04 12:07:05 +0200

    Fixes 'no db column' issues by force re-adding columns name, description and price to Product table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_140962) do
+ActiveRecord::Schema.define(version: 2019_05_04_095503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,9 @@ ActiveRecord::Schema.define(version: 2019_05_03_140962) do
     t.datetime "updated_at", null: false
     t.string "category"
     t.bigint "category_id"
+    t.string "name"
+    t.string "description"
+    t.integer "price"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
