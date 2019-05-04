319134bd588102f281152dbb2d6c64b7ec646f6a
zanenkn <zane.neikena@gmail.com>
2019-05-04 21:06:25 +0200

    Removes an ambiguous step definition about clicking a link

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -14,7 +14,4 @@ When("I fill in {string} with {string}") do |string, string2|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("I click on {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
 
